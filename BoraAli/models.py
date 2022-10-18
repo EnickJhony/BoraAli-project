8688fbac92381cf68b4672b150ded65786a5d454
enick <enick.jhony@gmail.com>
2022-10-18 20:59:56 -0300

    finalizando aula com o feedback do professor sobre o que falta

--- BoraAli/models.py
+++ BoraAli/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Trilha(models.model):
+    nome_trilha = models.CharField(max_length=200)
+    descricao = models.CharField(max_length=400)
+    localizacao = 
+    duracao = 
+    nivel = 
+    curiosidades = 
